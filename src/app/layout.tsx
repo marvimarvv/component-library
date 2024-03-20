d086ff2a7ee4fc5b813556cdd76e75517fd7a1dc
Marvin Tandler <mtandler@epages.com>
2024-03-20 22:04:54 +0100

    Update: Add some theme styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import "./globals.css";
 
-import { DotGothic16 } from "next/font/google";
+import { DotGothic16, Figtree } from "next/font/google";
+import { ThemeProvider, useTheme } from "../components/ThemeProvider";
+
 import type { Metadata } from "next";
-import { ThemeProvider } from "../components/ThemeProvider";
 
 const dotGothic16 = DotGothic16({ weight: "400", subsets: ["latin"] });
+const figtree = Figtree({ weight: "variable", subsets: ["latin"] });
 
-export const metadata: Metadata = {
+/* export const metadata: Metadata = {
   title: "Marvin's Component Library",
   description: "A collection of reusable components for Marvin's projects",
 };
+ */
 
 export default function RootLayout({
   children,
@@ -17,9 +22,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ThemeProvider>
+      <Layout>{children}</Layout>
+    </ThemeProvider>
+  );
+}
+
+function Layout({ children }: { children: React.ReactNode }) {
+  const { theme } = useTheme() || ({} as { theme: string }); // Set default value as empty object if useTheme() returns null
+
+  return (
     <html lang="en">
-      <body className={dotGothic16.className}>
-        <ThemeProvider>{children}</ThemeProvider>
+      <body
+        className={
+          theme === "gradient-theme" ? figtree.className : dotGothic16.className
+        }
+      >
+        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,19 @@ import MobileNav from "@/components/MobileNav";
 import ModeToggle from "@/components/ModeToggle";
 import ThemeToggle from "@/components/ThemeToggle";
 import { motion } from "framer-motion";
+import { useTheme } from "@/components/ThemeProvider";
 
 export default function Home() {
+  const { theme } = useTheme() || ({} as { theme: string });
+
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24 bg-skin-background">
+    <main
+      className="flex min-h-screen flex-col justify-between p-24 bg-skin-background"
+      style={{
+        backgroundImage:
+          "radial-gradient(at 8% 44%, hsla(204,86%,69%,1) 0px, transparent 50%), radial-gradient(at 70% 74%, hsla(37,96%,74%,1) 0px, transparent 50%), radial-gradient(at 3% 18%, hsla(308,75%,62%,1) 0px, transparent 50%), radial-gradient(at 34% 7%, hsla(222,85%,65%,1) 0px, transparent 50%), radial-gradient(at 75% 13%, hsla(257,85%,71%,1) 0px, transparent 50%), radial-gradient(at 58% 39%, hsla(264,98%,69%,1) 0px, transparent 50%), radial-gradient(at 37% 55%, hsla(264,67%,74%,1) 0px, transparent 50%);",
+      }}
+    >
       <MobileNav
         links={[
           { label: "Home", href: "/" },
@@ -18,7 +27,13 @@ export default function Home() {
         backgroundColor="skin-primary-500"
       />
       <h1 className="flex flex-col">
-        <span className="text-fluid-3xl md:text-[170px] text-skin-primary-500">
+        <span
+          className={`text-fluid-3xl md:text-[170px] text-skin-primary-500 ${
+            theme === "gradient-theme"
+              ? "text-transparent bg-clip-text bg-gradient-to-br from-skin-primary-100 via-skin-primary-500 to-skin-primary-100"
+              : ""
+          }`}
+        >
           Welcome
         </span>
         <span className="text-fluid-l text-skin-on-background">
