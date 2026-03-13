f2cf2eb20e00ee36fc82f2c5c1117c48fd13bce4
Marvin Tandler <mtandler@epages.com>
2026-03-13 17:41:54 +0100

    Improve styling structure

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -7,8 +7,53 @@ interface AccordionProps {
   onlyOneItemOpen?: boolean;
 }
 
+const accordionWrapperStyles = cva(
+  "gap-space-m grid grid-cols-1 max-w-md md:grid-cols-2 rounded-md w-full bg-primary-100 group",
+  {
+    variants: {
+      theme: {
+        "gradient-theme": ``,
+        "neon-theme": ``,
+      },
+    },
+    defaultVariants: {
+      theme: "gradient-theme",
+    },
+  },
+);
+
 const accordionEntryStyles = cva(
-  "border-primary-200 dark:border-primary-700 rounded-skin w-full",
+  "border border-primary-200 dark:border-primary-700 rounded-sm details-content:h-0 details-content:overflow-hidden details-content:transition-[height,content-visibility] open:details-content:h-auto details-content:transition-discrete w-full",
+  {
+    variants: {
+      theme: {
+        "gradient-theme": ``,
+        "neon-theme": ``,
+      },
+    },
+    defaultVariants: {
+      theme: "gradient-theme",
+    },
+  },
+);
+
+const accordionSummaryStyles = cva(
+  "cursor-pointer list-none px-space-s py-space-xs text-primary-900 dark:text-primary-100 font-medium",
+  {
+    variants: {
+      theme: {
+        "gradient-theme": ``,
+        "neon-theme": ``,
+      },
+    },
+    defaultVariants: {
+      theme: "gradient-theme",
+    },
+  },
+);
+
+const accordionDetailsStyles = cva(
+  "px-space-s py-space-xs text-primary-700 dark:text-primary-300",
   {
     variants: {
       theme: {
@@ -27,8 +72,7 @@ const accordionImageStyles = cva(
   {
     variants: {
       theme: {
-        "gradient-theme":
-          "from-accent-400 to-accent-600 bg-linear-to-r",
+        "gradient-theme": "from-accent-400 to-accent-600 bg-linear-to-r",
         "neon-theme": "bg-neon-theme-accent",
       },
     },
@@ -48,20 +92,18 @@ export const Accordion = ({
   };
 
   return (
-    <div className="gap-space-m grid grid-cols-1 max-w-md md:grid-cols-2 rounded-md w-full bg-primary-100">
-      <div className={`group ${accordionEntryStyles({ theme })}`}>
+    <div className={accordionWrapperStyles({ theme })}>
+      <div>
         {accordionEntries.map(({ details, summary }, index) => (
           <details
             key={index}
             {...(onlyOneItemOpen && { name: "accordion-item" })}
-            className="details-content:h-0 details-content:overflow-hidden details-content:transition-[height,content-visibility] open:details-content:h-auto details-content:transition-discrete w-full"
+            className={accordionEntryStyles({ theme })}
           >
-            <summary className="cursor-pointer font-medium px-space-s py-space-xs list-none ">
+            <summary className={accordionSummaryStyles({ theme })}>
               {summary}
             </summary>
-            <div className="dark:text-base-400 pb-space-s px-space-s text-base-500">
-              {details}
-            </div>
+            <div className={accordionDetailsStyles({ theme })}>{details}</div>
           </details>
         ))}
       </div>
@@ -71,6 +113,7 @@ export const Accordion = ({
             <img
               key={index}
               src={src}
+              // They are treated as decorative images, if they are not, consider passing a meaningful alt text
               alt={""}
               className={`object-cover rounded-skin h-full w-full transition-[opacity,transform] absolute inset-0 opacity-0 scale-110 group-has-[details:nth-child(${index + 1})[open]]:opacity-100 group-has-[details:nth-child(${index + 1})[open]]:scale-100`}
             />
